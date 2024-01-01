e1f841ae24e62c960f107e97e8ff7c6403171df9
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2024-01-02 04:18:37 +0700

    5

--- src/components/modal/Member/AddMember.jsx
+++ src/components/modal/Member/AddMember.jsx
@@ -119,7 +119,7 @@ const AddMember = ({ handleOpen, open, fetchData, dataToModal }) => {
   return (
     <Dialog open={open} size="sm" handler={handleOpen}>
       <DialogHeader className="bg-gray-200 rounded-lg flex gap-4 text-lg">
-        <HiOutlineUserGroup size={24} color="black" /> สร้างพนักงานใหม่
+        <HiOutlineUserGroup size={24} color="black" /> สร้างพนักงานใหม่.
       </DialogHeader>
       <DialogBody>
         {/* {JSON.stringify(sendData)} */}
